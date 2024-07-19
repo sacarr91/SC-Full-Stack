dba56731486753c0c1fc8358f47b4e438ddd2235
Stephanie Carr <sacarr91@gmail.com>
2024-07-19 09:18:14 -0400

    added link to header icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import NavTabs from "./NavTabs";
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 
 function Header() {
     return <>
     <Navbar fixed="top" expand="lg" className="bg-body-tertiary">
       <Container>
-      <img src='/src/assets/sc-portfolio-header-image.png' height={50}></img>
+      <Link to="/About"><img src='/src/assets/sc-portfolio-header-image.png' height={50}></img>
+      </Link>
       <NavTabs />
       </Container>
     </Navbar>
