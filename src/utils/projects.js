88a07b1de5804c7acbb8ca2d1356d9ba02118c9c
Stephanie Carr <sacarr91@gmail.com>
2024-07-19 10:53:07 -0400

    updated images source paths

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,7 +2,7 @@ const projectsList = [
     {
         id: 'tutorhub',
         title: 'TutorHub',
-        src: '/src/assets/tutorHub.gif',
+        src: '/images/tutorHub.gif',
         description: `Find a private teacher to help you along in your musical studies! Search for the right tutor with greater efficiency. Leverages PostgreSQL, JavaScript & Bootstrap. Developed with a team over 2-week sprint.`,
         link: 'https://google.com',
         repo: 'https://github.com/sacarr91/tutorHub'
@@ -10,7 +10,7 @@ const projectsList = [
     {
         id: 'roster',
         title: 'Roster Roundup',
-        src: '/src/assets/roster.png',
+        src: '/images/roster.png',
         description: `A CMS interface leveraging Node.js, Inquirer, and PostgreSQL.`,
         link: 'https://github.com/sacarr91/Roster-Roundup/blob/main/README.md',
         repo: 'https://github.com/sacarr91/Roster-Roundup'
@@ -18,7 +18,7 @@ const projectsList = [
     {
         id: 'ntsg',
         title: 'New to Strings Guide',
-        src: '/src/assets/coming-soon.png',
+        src: '/images/coming-soon.png',
         description: 'A grab-and-go reference and resource website for classroom Strings teachers of any experience level. Currently undergoing migration from 3rd-party to in-house hosting.',
         link: 'https://google.com',
         repo: 'https://github.com/sacarr91'
@@ -26,7 +26,7 @@ const projectsList = [
     {
         id: 'weather',
         title: 'Rain or Shine Weather Forecast',
-        src: '/src/assets/weather.gif',
+        src: '/images/weather.gif',
         description: `A simple weather forecast app, leveraging OpenWeather API`,
         link: 'https://google.com',
         repo: 'https://github.com/sacarr91/rain-or-shine-weather-forecast'
@@ -34,7 +34,7 @@ const projectsList = [
     {
         id: 'social',
         title: 'SQUADUP!',
-        src: '/src/assets/coming-soon.png',
+        src: '/images/coming-soon.png',
         description: 'A social media network prototype to connect with a like-minded learning community',
         link: 'https://google.com',
         repo: 'https://github.com/sacarr91/SQUADUP'
@@ -42,7 +42,7 @@ const projectsList = [
     {
         id: 'jenna',
         title: 'Jenna Vincitore, MME, M.Ed.',
-        src: '/src/assets/coming-soon.png',
+        src: '/images/coming-soon.png',
         description: 'An online portfolio for a professional musician, music teacher & public school administrative candidate',
         link: 'https://google.com',
         repo: 'https://github.com/sacarr91'
