c6ca4594f5932e0fae2b3d14e674e983c59e1f96
Stephanie Carr <sacarr91@gmail.com>
2024-07-19 09:39:05 -0400

    learned: vp css is references as vh or vw (vmin and vmax also available)

--- src/pages/About-Me.jsx
+++ src/pages/About-Me.jsx
@@ -2,27 +2,26 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
+import '../utils/note.css'
 
 function AboutMe() {
-
     return (
         <>
             <Container className="py-5">
                 <center><img src='/src/assets/about.png' alt="about me" height="100"></img></center>
-                <Row style={{ height: "500px" }}>
-                    <Col xs={6} md={4}>
-                        <Image src="https://media.licdn.com/dms/image/D4E03AQHyucfFlfPM_w/profile-displayphoto-shrink_800_800/0/1716413707355?e=1726704000&v=beta&t=CAgYoBD_AUCXmWG12QJxjvD1c9gzMbkePWetY2fxP9Q" thumbnail />
-                    </Col>
-                    <Col xs={6} md={8}>
-                        <Image src="/src/assets/note.png" height={450} />
-                    </Col>
-
+                <Row>
+                    <Container >
+                        <Row>
+                            <Col xs={6} md={4}>
+                                <Image src="https://media.licdn.com/dms/image/D4E03AQHyucfFlfPM_w/profile-displayphoto-shrink_800_800/0/1716413707355?e=1726704000&v=beta&t=CAgYoBD_AUCXmWG12QJxjvD1c9gzMbkePWetY2fxP9Q" thumbnail />
+                            </Col>
+                            <Col xs={6} md={8}>
+                                <Image src="/src/assets/note.png" className='note' />
+                            </Col>
+                        </Row>
+                    </Container>
                 </Row>
             </Container>
-            <div>
-
-                <img sheight={200} className="m-3"></img>
-            </div>
         </>
     )
 };
